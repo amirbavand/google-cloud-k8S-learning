f36d9c173b10f97f012cddc86c815808a242d1b8
amirbavand <amirhossein.bavand@gmail.com>
2021-01-13 18:32:20 -0500

    some changes

--- server/app.py
+++ server/app.py
@@ -17,7 +17,7 @@ from base64 import encodebytes
 # from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@postgres/user-login-inforamation'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@postgres/user-login-inforamation1'
 db = SQLAlchemy(app)
 
 app.config['SECRET_KEY'] = 'secretkey'
