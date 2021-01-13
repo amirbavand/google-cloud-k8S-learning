10a6f1fcaf6b74801d8cda9d21d7edcd5193419f
amirbavand <amirhossein.bavand@gmail.com>
2021-01-13 11:55:18 -0500

    convert to pythin

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
-docker build -t ahbavand/multi-client:latest -t ahbavand/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ahbavand/multi-server:latest -t ahbavand/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ahbavand/multi-worker:latest -t ahbavand/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t ahbavand/python-client:latest -t ahbavand/python-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ahbavand/python-server:latest -t ahbavand/python-server:$SHA -f ./server/Dockerfile ./server
 
-docker push ahbavand/multi-client:latest
-docker push ahbavand/multi-server:latest
-docker push ahbavand/multi-worker:latest
+docker push ahbavand/python-client:latest
+docker push ahbavand/python-server:latest
+docker push ahbavand/python-worker:latest
 
-docker push ahbavand/multi-client:$SHA
-docker push ahbavand/multi-server:$SHA
-docker push ahbavand/multi-worker:$SHA
+docker push ahbavand/python-client:$SHA
+docker push ahbavand/python-server:$SHA
+docker push ahbavand/python-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ahbavand/multi-server:$SHA
-kubectl set image deployments/client-deployment client=ahbavand/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ahbavand/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=ahbavand/python-server:$SHA
+kubectl set image deployments/client-deployment client=ahbavand/python-client:$SHA
