4969814901f46ae63af621c8f5924e3e9f64a394
amirbavand <amirhossein.bavand@gmail.com>
2021-01-21 23:02:20 -0500

    small change in client

--- client/src/containers/register.js
+++ client/src/containers/register.js
@@ -8,6 +8,8 @@ class RegisterPage extends Component {
     username: '',
     password: '',
     isPublic: true,
+    status: ''
+
 
   };
 
@@ -34,9 +36,13 @@ class RegisterPage extends Component {
         console.log("password_is_valid");
         try {
           const values=await axios.post('api/register',{"userName": username, "password": password, "isPublic":this.state.isPublic} );
+          this.setState({status:"successful"})
+
     
         } catch (err) {
           console.log("the username already exists")
+          this.setState({status:"some information was wrong or error accourd"})
+
     
     
         
@@ -83,7 +89,7 @@ class RegisterPage extends Component {
           <input type="submit" value="Submit" />
         </form>
       <div>
-          <h1>{this.state.index}</h1>
+        <h1>{this.state.status}</h1>
         </div>
       </div>
 
